1794e15c00ed4cd2aac8c2e516110e2d43ee3290
songluyi <slysly759@gmail.com>
2016-09-21 16:30:47 +0800

    新增一点内容

--- Get_My_Proxy/get_proxy_start.py
+++ Get_My_Proxy/get_proxy_start.py
@@ -10,4 +10,27 @@ Contact:    slysly759@gmail.com
 -------------------------------------------------------------------------------
 """
 import requests
+class proxy_spider(object):
+    def __init__(self):
+        self.row_url={
+            '快代理':['http://www.kuaidaili.com/proxylist/'],
+            '西刺代理':['http://www.xicidaili.com/nn/','http://www.xicidaili.com/nt/',]
+        }
 
+    def form_url(self,row_url):
+        for i in row_url:
+            print(i)
+            return list(map(lambda x:print(i+str(x)),range(10)))
+    #看可否用lambda改写
+
+
+
+
+
+if __name__=='__main__':
+    fuck_proxy=proxy_spider()
+    row_url_1=fuck_proxy.row_url['快代理']
+    row_url_2=fuck_proxy.row_url['西刺代理']
+    url_1_list=fuck_proxy.form_url(row_url_1)
+    url_2_list=fuck_proxy.form_url(row_url_2)
+    print(url_1_list)
